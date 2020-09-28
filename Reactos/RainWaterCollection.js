0393de2ed2028392643fa8dd7e286b230e08f0cd
Qi Lin <qlin1996@gmail.com>
2020-09-28 12:49:54 -0400

    started rain water collection

---
+++ Reactos/RainWaterCollection.js
@@ -0,0 +1,6 @@
+// You are collecting rain water from pockets in between pillars. You are given an array of non-negative integers where each non-zero integer represents the height of a pillar of width 1. Write a function that returns the surface area of the water trapped between the pillars viewed from the front.
+
+/*
+Example
+Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
+*/
