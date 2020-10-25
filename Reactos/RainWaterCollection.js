34e2280bdc184b8c7c0919d1aaaeb4a80f2bc8c8
Qi Lin <qlin1996@gmail.com>
2020-10-25 18:49:17 -0400

    added rain water collection

--- Reactos/RainWaterCollection.js
+++ Reactos/RainWaterCollection.js
@@ -4,3 +4,25 @@
 Example
 Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
 */
+
+function rainCollector(blocks) {
+  const rightMaxes = [];
+  let rightMax = 0;
+  for (let i = blocks.length - 1; i >= 0; i--) {
+    rightMax = Math.max(rightMax, blocks[i]);
+    rightMaxes[i] = rightMax;
+  }
+
+  const leftMaxes = [];
+  let leftMax = 0;
+  for (let i = 0; i < blocks.length; i++) {
+    leftMax = Math.max(leftMax, blocks[i]);
+    leftMaxes[i] = leftMax;
+  }
+
+  return blocks.reduce((waterCollected, block, idx) => {
+    const leftMax = leftMaxes[idx];
+    const rightMax = rightMaxes[idx];
+    return waterCollected + Math.min(leftMax, rightMax) - block;
+  }, 0);
+}
