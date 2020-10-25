137be8f5a077ea037d57cd122e9f409a1b9be319
Qi Lin <qlin1996@gmail.com>
2020-10-25 15:23:52 -0400

    added min size subarray sum

---
+++ Others/MinSizeSubArraySum.js
@@ -0,0 +1,34 @@
+/*
+Given an array of n positive integers and a positive integer s, find the minimal length of a contiguous subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+
+Example:
+
+Input: s = 7, nums = [2,3,1,2,4,3]
+Output: 2
+Explanation: the subarray [4,3] has the minimal length under the problem constraint.
+*/
+
+function minSubArrayLen(nums, sum) {
+  let total = 0;
+  let start = 0;
+  let end = 0;
+  let minLen = Infinity;
+  while (start < nums.length) {
+    // if current window doesn't add up to the given sum then move the window to right
+    if (total < sum && end < nums.length) {
+      total += nums[end];
+      end++;
+    }
+    // if current window adds up to at least the sum given then we can shrink the window
+    else if (total >= sum) {
+      minLen = Math.min(minLen, end - start);
+      total -= nums[start];
+      start++;
+    }
+    // current total less than required total but we reach the end, need this or else we'll be in an infinite loop
+    else {
+      break;
+    }
+  }
+  return minLen === Infinity ? 0 : minLen;
+}
