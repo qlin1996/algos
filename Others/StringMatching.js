9bf1932dd272ad6b79262aba5cd7cee1e39b8273
Qi Lin <qlin1996@gmail.com>
2020-10-25 18:48:42 -0400

    added string matching

--- Others/StringMatching.js
+++ Others/StringMatching.js
@@ -2,3 +2,18 @@
 
 // Example
 // ["mass","as","hero","superhero"] should return ["as","hero"]
+
+var stringMatching = function (array) {
+  return array.filter((word, idx) => {
+    for (let i = 0; i < array.length; i++) {
+      if (i === idx || word.length > array[i].length) continue;
+      if (subSection(array[i], word)) return true;
+    }
+    return false;
+  });
+};
+
+function subSection(word, subword) {
+  let array = word.split(subword);
+  return array.length > 1;
+}
